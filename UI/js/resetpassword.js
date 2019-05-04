8e7aa700382048e42d021caf0606ea54f6db26fc
Ibukunoluwa Adeeko <ibukunadeeko@gmail.com>
2019-05-04 16:26:32 +0100

    [feature #165794584] Reset Password Page

---
+++ UI/js/resetpassword.js
@@ -0,0 +1,46 @@
+const reset = document.getElementById('reset-password');
+const forgotPassword = document.getElementById('forgot-password');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const confirmPassword = document.getElementById('confirm-password');
+const message = document.querySelector('.offset');
+
+// Retrieve token from url
+// const urlParams = new URLSearchParams(window.location.search);
+// const resetToken = urlParams.get('token');
+
+// Check password
+const check = () => {
+  if (password.value !== confirmPassword.value) {
+    message.style.color = 'red';
+    message.innerHTML = 'Passwords do not match';
+    return false;
+  } return true;
+};
+
+if (password && confirmPassword) {
+  [password, confirmPassword].forEach((elem) => {
+    elem.addEventListener('input', () => {
+      message.innerHTML = '';
+    });
+  });
+}
+
+if (reset) {
+  reset.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const matching = check();
+    if (matching) {
+      // Do something
+    }
+  }); 
+}
+
+if (forgotPassword) {
+  forgotPassword.addEventListener('submit', (e) => {
+    e.preventDefault();
+    // Do Something
+  });
+}
+
+
