5e30009715ca1a7589883820c177a56f3aa1b2eb
Ibukunoluwa Adeeko <ibukunadeeko@gmail.com>
2019-05-16 13:32:24 +0100

    [chore #166057248] Setup PostgreSQL Database

---
+++ server/db/index.js
@@ -0,0 +1,29 @@
+import { Pool, types } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const connectionString = process.env.NODE_ENV === 'test' ? process.env.TESTDB : process.env.DATABASE_URL;
+
+types.setTypeParser(1700, val => parseFloat(val));
+
+const pool = new Pool({ connectionString });
+
+export default {
+  /**
+   * Query Database
+   * @param {string} text - Query Text
+   * @param {array} params - Query Values
+   */
+  query(text, params) {
+    return new Promise((resolve, reject) => {
+      pool.query(text, params)
+        .then((res) => {
+          resolve(res);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  },
+};
---
+++ server/test/testData/createTables.js
@@ -0,0 +1,48 @@
+import db from '../../db';
+
+const createTables = `
+  CREATE TABLE IF NOT EXISTS users (
+    id SERIAL NOT NULL,
+    firstName VARCHAR(50) NOT NULL,
+    lastName VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(128) NOT NULL,
+    phone VARCHAR(11) NOT NULL UNIQUE,
+    status VARCHAR(10) NOT NULL CHECK(status IN ('verified', 'unverified')) DEFAULT 'unverified',
+    isAdmin BOOLEAN NOT NULL DEFAULT false,
+    registered DATE NOT NULL DEFAULT CURRENT_DATE,
+    PRIMARY KEY (id, email)
+  );
+
+  CREATE TABLE IF NOT EXISTS loans (
+    id SERIAL NOT NULL PRIMARY KEY,
+    firstname VARCHAR(50) NOT NULL,
+    lastname VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL REFERENCES users(email),
+    amount NUMERIC NOT NULL CHECK(10000 <= amount AND amount <= 200000),
+    tenor NUMERIC NOT NULL CHECK(0 < tenor AND tenor <= 12),
+    status VARCHAR(10) NOT NULL CHECK(status IN ('pending', 'approved', 'rejected')) DEFAULT 'pending',
+    repaid BOOLEAN NOT NULL DEFAULT false,
+    paymentInstallment NUMERIC NOT NULL,
+    balance NUMERIC NOT NULL,
+    interest NUMERIC NOT NULL,
+    createdOn DATE NOT NULL DEFAULT CURRENT_DATE
+  );
+  
+  CREATE TABLE IF NOT EXISTS repayments (
+    id SERIAL NOT NULL PRIMARY KEY,
+    loanid INT NOT NULL REFERENCES loans(id),
+    amount NUMERIC NOT NULL CHECK(amount > 0),
+    createdon DATE NOT NULL DEFAULT CURRENT_DATE
+  );
+`;
+
+const createDatabaseTables = async () => {
+  await db.query(createTables).then(() => {
+    console.log('Tables successfully created');
+  });
+};
+
+before(async () => {
+  await createDatabaseTables();
+});
