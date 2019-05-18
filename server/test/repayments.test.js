7bab57a720252f6de6c4a61d7046ce065917e830
Ibukunoluwa Adeeko <ibukunadeeko@gmail.com>
2019-05-18 12:23:05 +0100

    ch(implementFeedback): refactor repayments test
    Refactored repayments.test.js file to use async/await syntax

--- server/test/repayments.test.js
+++ server/test/repayments.test.js
@@ -7,141 +7,99 @@ import app from '../app';
 chai.use(chaiHttp);
 chai.should();
 
+let request;
+beforeEach(() => {
+  request = chai.request(app);
+});
+
 describe('POST /loans/:id/repayment', () => {
-  it('should not make payment if ID is invalid', (done) => {
+  it('should not make payment if ID is invalid', async () => {
     const id = 'abc';
-    chai.request(app)
-      .post(`/api/v1/loans/${id}/repayment`)
-      .send({ amount: 10000 })
-      .end((err, res) => {
-        res.should.have.status(400);
-        res.body.should.be.a('object');
-        res.body.should.have.property('error');
-        res.body.should.have.property('status').eql(400);
-        done(err);
-      });
+    const res = await request.post(`/api/v1/loans/${id}/repayment`).send({ amount: 10000 });
+    res.should.have.status(400);
+    res.body.should.be.a('object');
+    res.body.should.have.property('error');
+    res.body.should.have.property('status').eql(400);
   });
-  it('Should not make Payment if amount is invalid', (done) => {
+  it('Should not make Payment if amount is invalid', async () => {
     const id = 7;
-    chai.request(app)
-      .post(`/api/v1/loans/${id}/repayment`)
-      .send({ amount: '234ed$' })
-      .end((err, res) => {
-        res.should.have.status(400);
-        res.body.should.be.a('object');
-        res.body.should.have.property('error');
-        res.body.should.have.property('status').eql(400);
-        done(err);
-      });
+    const res = await request.post(`/api/v1/loans/${id}/repayment`).send({ amount: '234ed$' });
+    res.should.have.status(400);
+    res.body.should.be.a('object');
+    res.body.should.have.property('error');
+    res.body.should.have.property('status').eql(400);
   });
-  it('It should not Make Payment if Loan is not approved', (done) => {
+  it('It should not Make Payment if Loan is not approved', async () => {
     const id = 11;
-    chai.request(app)
-      .post(`/api/v1/loans/${id}/repayment`)
-      .send({ amount: 40000 })
-      .end((err, res) => {
-        res.should.have.status(400);
-        res.body.should.be.a('object');
-        res.body.should.have.property('error');
-        res.body.should.have.property('status').eql(400);
-        done(err);
-      });
+    const res = await request.post(`/api/v1/loans/${id}/repayment`).send({ amount: 40000 });
+    res.should.have.status(400);
+    res.body.should.be.a('object');
+    res.body.should.have.property('error');
+    res.body.should.have.property('status').eql(400);
   });
-  it('should not make payment if loan has been fully repaid', (done) => {
+  it('should not make payment if loan has been fully repaid', async () => {
     const id = 8;
-    chai.request(app)
-      .post(`/api/v1/loans/${id}/repayment`)
-      .send({ amount: 40000 })
-      .end((err, res) => {
-        res.should.have.status(400);
-        res.body.should.be.a('object');
-        res.body.should.have.property('error');
-        res.body.should.have.property('status').eql(400);
-        done(err);
-      });
+    const res = await request.post(`/api/v1/loans/${id}/repayment`).send({ amount: 40000 });
+    res.should.have.status(400);
+    res.body.should.be.a('object');
+    res.body.should.have.property('error');
+    res.body.should.have.property('status').eql(400);
   });
-  it('should not make payment if Loan with given ID does not exist', (done) => {
+  it('should not make payment if Loan with given ID does not exist', async () => {
     const id = 999;
-    chai.request(app)
-      .post(`/api/v1/loans/${id}/repayment`)
-      .send({ amount: 40000 })
-      .end((err, res) => {
-        res.should.have.status(404);
-        res.body.should.be.a('object');
-        res.body.should.have.property('error');
-        res.body.should.have.property('status').eql(404);
-        done(err);
-      });
+    const res = await request.post(`/api/v1/loans/${id}/repayment`).send({ amount: 40000 });
+    res.should.have.status(404);
+    res.body.should.be.a('object');
+    res.body.should.have.property('error');
+    res.body.should.have.property('status').eql(404);
   });
-  it('should make Payment if all checks are passed', (done) => {
+  it('should make Payment if all checks are passed', async () => {
     const id = 10;
     const amount = 2724.39;
-    chai.request(app)
-      .post(`/api/v1/loans/${id}/repayment`)
-      .send({ amount })
-      .end((err, res) => {
-        res.should.have.status(200);
-        res.body.should.be.a('object');
-        res.body.data.should.have.property('loanId').eql(id);
-        res.body.data.should.have.property('paidAmount').eql(amount);
-        res.body.data.should.have.property('amount');
-        res.body.data.should.have.property('balance');
-        done(err);
-      });
+    const res = await request.post(`/api/v1/loans/${id}/repayment`).send({ amount });
+    res.should.have.status(200);
+    res.body.should.be.a('object');
+    res.body.data.should.have.property('loanId').eql(id);
+    res.body.data.should.have.property('paidAmount').eql(amount);
+    res.body.data.should.have.property('amount');
+    res.body.data.should.have.property('balance');
   });
-  it('should make Payments and change repaid to true when Loan payment is complete', (done) => {
+  it('should make Payments and change repaid to true when Loan payment is complete', async () => {
     const id = 16;
     const amount = 155042;
-    chai.request(app)
-      .post(`/api/v1/loans/${id}/repayment`)
-      .send({ amount })
-      .end((err, res) => {
-        res.should.have.status(200);
-        res.body.should.be.a('object');
-        res.body.data.should.have.property('loanId').eql(id);
-        res.body.data.should.have.property('paidAmount').eql(amount);
-        res.body.data.should.have.property('amount');
-        res.body.data.should.have.property('balance');
-        res.body.data.should.have.property('repaid').eql(true);
-        done(err);
-      });
+    const res = await request.post(`/api/v1/loans/${id}/repayment`).send({ amount });
+    res.should.have.status(200);
+    res.body.should.be.a('object');
+    res.body.data.should.have.property('loanId').eql(id);
+    res.body.data.should.have.property('paidAmount').eql(amount);
+    res.body.data.should.have.property('amount');
+    res.body.data.should.have.property('balance');
+    res.body.data.should.have.property('repaid').eql(true);
   });
 });
 
 describe('GET /loans/:id/repayments', () => {
-  it('Should NOT return list of repayments if ID is invalid', (done) => {
+  it('Should NOT return list of repayments if ID is invalid', async () => {
     const id = 'etc';
-    chai.request(app)
-      .get(`/api/v1/loans/${id}/repayments`)
-      .end((err, res) => {
-        res.should.have.status(400);
-        res.body.should.have.property('error');
-        res.body.should.have.property('status').eql(400);
-        done(err);
-      });
+    const res = await request.get(`/api/v1/loans/${id}/repayments`);
+    res.should.have.status(400);
+    res.body.should.have.property('error');
+    res.body.should.have.property('status').eql(400);
   });
-  it('Should NOT return list of repayments if ID is not found', (done) => {
+  it('Should NOT return list of repayments if ID is not found', async () => {
     const id = 999;
-    chai.request(app)
-      .get(`/api/v1/loans/${id}/repayments`)
-      .end((err, res) => {
-        res.should.have.status(404);
-        res.body.should.have.property('error');
-        res.body.should.have.property('status').eql(404);
-        done(err);
-      });
+    const res = await request.get(`/api/v1/loans/${id}/repayments`);
+    res.should.have.status(404);
+    res.body.should.have.property('error');
+    res.body.should.have.property('status').eql(404);
   });
-  it('Should return list of repayments', (done) => {
+  it('Should return list of repayments', async () => {
     const id = 10;
-    chai.request(app)
-      .get(`/api/v1/loans/${id}/repayments`)
-      .end((err, res) => {
-        res.should.have.status(200);
-        res.body.should.have.property('data');
-        res.body.data.should.be.a('array');
-        res.body.data[0].should.have.property('loanId').eql(id);
-        res.body.data[0].should.have.property('amount');
-        done(err);
-      });
+    const res = await request.get(`/api/v1/loans/${id}/repayments`);
+    res.should.have.status(200);
+    res.body.should.have.property('data');
+    res.body.data.should.be.a('array');
+    res.body.data[0].should.have.property('loanId').eql(id);
+    res.body.data[0].should.have.property('amount');
   });
 });
