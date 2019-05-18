3e7f7001303c5d46d95a1e7d7edcedd63ea01a70
Ibukunoluwa Adeeko <ibukunadeeko@gmail.com>
2019-05-18 18:27:57 +0100

    bg(fixTravis): fix travis build

--- server/models/loanModel.js
+++ server/models/loanModel.js
@@ -77,8 +77,7 @@ class LoanModel {
       let queryText;
       if (max) {
         queryText = `
-        INSERT INTO loans (firstname, lastname, email, amount, tenor, status, repaid, paymentinstallment, balance, interest) 
-        VALUES  ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) 
+        INSERT INTO loans (firstname, lastname, email, amount, tenor, status, repaid, paymentinstallment, balance, interest) VALUES  ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) 
         ON CONFLICT (id) DO UPDATE 
         SET (id, firstname, lastname, email, amount, tenor, status, repaid, paymentinstallment, balance, interest, createdon) = (${newID}, EXCLUDED.firstname, EXCLUDED.lastname, EXCLUDED.email, EXCLUDED.amount, EXCLUDED.tenor, EXCLUDED.status, EXCLUDED.repaid, EXCLUDED.paymentinstallment, EXCLUDED.balance, EXCLUDED.interest, EXCLUDED.createdon) RETURNING *`;
       } else {
